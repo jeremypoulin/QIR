a6a506a72d22ec5cd2a07f16bf15f22b65b10536
Lucas Robson <returnnada@gmail.com>
2025-01-05 18:15:28 -0500

    Made pause menu + mouseless menu

--- scripts/Modes.gd
+++ scripts/Modes.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var local: Button = $MarginContainer/VBoxContainer/Local
+
+func _ready() -> void:
+	local.grab_focus()
+
 func _on_local_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/node_2d.tscn")
 
