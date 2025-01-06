c2f62fa7512ac3a54a489bbb5217654fe5df83d4
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-01-06 00:13:30 -0500

    State Machine fully outlined

--- scenes/state_machine.gd
+++ scenes/state_machine.gd
@@ -1,8 +1,18 @@
 extends Node
 
+var current_state = State
 var states : Dictionary = {}
 
 func _ready():
 	for child in get_children():
 		if child is State:
 			states[child.name] = child
+			child.Transitioned.connect(on_child_transition)
+
+func _process(delta):
+	if current_state:
+		current_state._update(delta)
+		
+func _physics_process(delta):
+	if current_state:
+		current_state._physics_update(delta)
